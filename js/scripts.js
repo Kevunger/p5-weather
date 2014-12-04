36534a31d2a1dbb9525042e02ca9abdaf29c6949
Kevunger <kevinungerecht@gmail.com>
2014-12-03 18:52:57 -0800

    Version -> 4.2
    Restyled zip code search
    Wrote script for zip code search

--- js/scripts.js
+++ js/scripts.js
@@ -40,3 +40,28 @@ function loadWeather(location, woeid) {
   });
 }
 
+$('.search').on('click', function() {
+  
+  var zipcode = $('input').val();
+  console.log(zipcode);
+  
+  $.simpleWeather({
+    location: zipcode,
+    success: function(weather) {
+      html = '<div class="place"><p class="city">'+weather.city+', '+weather.region+'</p></div><br><hr class="rule"></hr>';
+      html += '<div class="currently"><i class="icon-'+weather.code+'"></i>&nbsp;&nbsp;'+weather.currently+'</div>';
+      html += '<div class="ui horizontal list"><div class="low item"><i class="chevron circle down icon"></i>'+weather.low+'&deg;</div>';
+      html += '<div class="currTemp item">'+weather.temp+'&deg;</div>';
+      html += '<div class="high item">'+weather.high+'&deg;<i class="chevron circle up icon"></i></div></div>';
+      html += '<div class="ui sun list"><div class="item"><p><i class="sun icon"></i>Sun</p><hr class="rule"></hr></div><div class="item"><i class="double angle up icon"></i>'+weather.sunrise+'</div>';
+      html += '<div class="item"><i class="double angle down icon"></i>'+weather.sunset+'</div></div>';
+      html += '<div class="ui wind list"><div class="item"><p><i class="flag outline icon"></i>Wind</p><hr class="rule"></hr></div><div class="item">'+weather.wind.direction+'&nbsp;'+weather.wind.speed+' '+weather.units.speed+'</div><div class="item">'+weather.wind.chill+'&deg;</div></div>';
+      
+      $("#weather").html(html);
+    },
+    error: function(error) {
+      $('#weather').html('<p>' + error + '</p>');
+    }
+  });
+  $('#getWeather').val('');
+});
